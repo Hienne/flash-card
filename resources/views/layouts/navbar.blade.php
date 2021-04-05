b560e90848cfc56527b032dab68a78af56c34544
= <=>
2021-04-05 23:02:01 +0700

    update dropdown folder and subject in nav

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -7,6 +7,37 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Trang chủ</a>
       </li>
+
+      @auth
+        <!-- Dropdown  Folder-->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+          Thư mục
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item dropdown__item--size" href="#">Thư mục 1</a>
+          <a class="dropdown-item dropdown__item--size" href="#">Thư mục 2</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item dropdown__item--size" href="#">Tất cả thư mục</a>
+        </div>
+      </li>
+
+      <!-- Dropdown Subject-->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+          Học phần
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item dropdown__item--size" href="#">Học phần 1</a>
+          <a class="dropdown-item dropdown__item--size" href="#">Học phần 2</a>
+          <a class="dropdown-item dropdown__item--size" href="#">Học phần 3</a>
+          <a class="dropdown-item dropdown__item--size" href="#">Học phần 4</a>
+          <a class="dropdown-item dropdown__item--size" href="#">Học phần 5</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item dropdown__item--size" href="#">Tất cả học phần</a>
+        </div>
+      </li>
+      @endauth
   
       <!-- Dropdown -->
       <li class="nav-item dropdown">
